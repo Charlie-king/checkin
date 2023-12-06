9f9a300aa2c57268c3dd93ac193978688e27cd5e
Charlie-king <jinbo0918@163.com>
2023-12-06 11:38:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 import requests, json, re, os
 
-session = requests.session()
-# 机场的地址
-url = os.environ.get('URL')
-# 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
-# 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
-# server酱
-SCKEY = os.environ.get('SCKEY')
 
-login_url = '{}/auth/login'.format(url)
-check_url = '{}/user/checkin'.format(url)
+# 从环境变量获取参数
+URL = os.environ.get('URL')
+ACCOUNTS = json.loads(os.environ.get('ACCOUNTS'))  
+PUSH_TOKEN = os.environ.get('PUSH_TOKEN')
 
+session = requests.Session()
 
-header = {
-        'origin': url,
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
-data = {
-        'email': email,
-        'passwd': passwd
-}
-try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
-    # 进行签到
-    result = json.loads(session.post(url=check_url,headers=header).text)
-    print(result['msg'])
-    content = result['msg']
-    # 进行推送
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
-        print('推送成功')
-except:
-    content = '签到失败'
-    print(content)
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
+def sign(email, passwd):
+  login_url = f"{URL}/auth/login"
+  check_url = f"{URL}/user/checkin"
+
+  data = {"email": email, "passwd": passwd}
+  resp = session.post(login_url, json=data)
+
+  resp = session.post(check_url)
+  return resp.json()["msg"]
+
+results = {}
+for account in ACCOUNTS:
+  email = account["email"]
+  passwd = account["passwd"]  
+
+  result = sign(email, passwd)
+  results[email] = result
+
+  print(f"{email} 签到结果: {result}")
+
+desp = ""  
+for email, status in results.items():
+  desp += f"{email} 签到结果:{status}\n"
+
+if PUSH_TOKEN:
+  # 推送代码
+
+print("签到任务完成")
